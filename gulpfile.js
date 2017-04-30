db14d0255894af9ca63b64d4515df2fa78891514
luuaAr26 <raul_cosmin_26@yahoo.com>
2017-04-30 17:19:52 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+/**
+ * Created by raul_ on 30-Apr-17.
+ */
+const gulp = require('gulp');
+
+gulp.task('Hello', function () {
+  console.log('Hello world');
+});
+
+gulp.task('default', ['Hello']);
---
+++ js/index.js
@@ -0,0 +1,4 @@
+(function () {
+  console.log('works');
+  alert('It works');
+}());
